0aafab5059d1e8cc2ee9185620add5b38ce9d190
Angel Zaprianov <fire1@abv.bg>
2019-09-17 12:46:08 +0300

    init

--- cmake-build-debug/CMakeFiles/kLineCan-size.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/kLineCan-size"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/kLineCan-size.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
